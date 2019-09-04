10fb0683dab39e7984895884f62df21774ec1b25
Nathan Fritz <nfritz2016@gmail.com>
2019-09-04 14:20:15 -0500

    finished unit testing for one case. Run it 10 times to meet requirement

--- src/RNG_Testing.java
+++ src/RNG_Testing.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,6 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class RNG_Testing {
     @Test
     public void rng_test() {
-
+        RNG rng = new RNG(1000000);
+        int[] rnums = new int[1000];
+        int cur = 0;
+        int random = rng.generate();
+        int duplicates = 0;
+        while (cur < 1000) {
+            rnums[cur] = random;
+            for (int j = 0; j <= cur - 1; j++) {
+                for (int k = j + 1; k <= cur; k++) {
+                    if (rnums[j] == rnums[k]) {
+                        duplicates++;
+                    }
+                }
+            }
+            random = rng.generate();
+            cur++;
+        }
+        Assert.assertTrue(duplicates <= 250);
     }
 }
