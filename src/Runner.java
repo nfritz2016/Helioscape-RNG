dd2cf776eaa5cfd511c1716fb93db60a46218664
Nathan Fritz <nfritz2016@gmail.com>
2019-09-04 13:48:24 -0500

    added logging

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,29 @@
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Runner {
   public static void main(String[] args) {
+    Logger logger = Logger.getLogger("appLogger");
+    FileHandler fh;
+    try {
+      fh = new FileHandler("rng.log");
+      logger.addHandler(fh);
+      SimpleFormatter formatter = new SimpleFormatter();
+      fh.setFormatter(formatter);
+    }
+    catch (SecurityException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    logger.info("Generating random number...");
     RNG rng = new RNG(1000000);
-    System.out.println(rng.generate());
+    int num = rng.generate();
+    logger.info("Generated: " + num);
+    System.out.println(num);
   }
 }
