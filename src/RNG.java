76806d3d0715e3257f66d78afd04d3e531edad7d
Nathan Fritz <nfritz2016@gmail.com>
2019-09-11 12:10:05 -0500

    Delete RNG.java

--- src/RNG.java
+++
@@ -1,9 +0,0 @@
-public class RNG {
-    int max;
-    public RNG(int max) {
-        this.max = max;
-    }
-    public int generate() {
-        return (int)(Math.random() * this.max + 1);
-    }
-}
